8d597ef22037dd40298208b40088f89e39f4c73f
KusalDemo <kusaldemo2022@gmail.com>
2024-07-18 16:02:35 +0530

    Implemented Quiz Iteration.

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
 const questions =[
     {
-        question : "Kusal Gunasekara",
+        question : "Kusal Gunasekara1",
         answer : [{"Answer":"Kusal Gunasekara","correct":true}, {"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":false}],
+    },
+    {
+        question : "Kusal Gunasekara2",
+        answer : [{"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":true}, {"Answer":"Kusal Gunasekara","correct":false}],
+    },
+    {
+        question : "Kusal Gunasekara3",
+        answer : [{"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":true}, {"Answer":"Kusal Gunasekara","correct":false}],
+    },
+    {
+        question : "Kusal Gunasekara4",
+        answer : [{"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":true}, {"Answer":"Kusal Gunasekara","correct":false}],
+    },
+    {
+        question : "Kusal Gunasekara5",
+        answer : [{"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":false}, {"Answer":"Kusal Gunasekara","correct":true}, {"Answer":"Kusal Gunasekara","correct":false}],
     }
 
 ]
+
+var callCount = 0;
 $(document).ready(() => {
+    const runQuiz = () => {
+        callCount++;
+        if (callCount > 5) {
+            console.log("Quiz Over")
+            return;
+        } else {
+            let timeoutId = setTimeout(runQuiz, 2000);
+        }
+        quizCardLoader(callCount-1);
+    };
+
+    runQuiz();
+});
+
+let quizCardLoader= (number)=>{
     $('#question').html("");
-    for (let i = 0; i < 1; i++) {
-        $("#questionNumber").text(i+1+" / 5 Question")
-        console.log("I'm Running..");
-        let question = questions[i].question;
-        let questionTemp = `
-            <h4>${i+1}. ${question}</h4><br>
+    $("#questionNumber").text(number+1+" / 5 Question");
+    let question = questions[number].question;
+    let questionTemp = `
+            <h4>${number + 1}. ${question}</h4><br>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="answer" id="answer${i+1}">
-              <label class="form-check-label" for="answer${i+1}">
-                ${questions[i].answer[0].Answer}
+              <input class="form-check-input" type="radio" name="answer" id="answer${number + 1}">
+              <label class="form-check-label" for="answer${number + 1}">
+                ${questions[number].answer[0].Answer}
               </label>
-            </div>
+            </div><br>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="answer" id="answer${i+2}">
-              <label class="form-check-label" for="answer${i+2}">
-                ${questions[i].answer[1].Answer}
+              <input class="form-check-input" type="radio" name="answer" id="answer${number + 2}">
+              <label class="form-check-label" for="answer${number + 2}">
+                ${questions[number].answer[1].Answer}
               </label>
-            </div>
+            </div><br>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="answer" id="answer${i+3}">
-              <label class="form-check-label" for="answer${i+3}">
-                ${questions[i].answer[2].Answer}
+              <input class="form-check-input" type="radio" name="answer" id="answer${number + 3}">
+              <label class="form-check-label" for="answer${number + 3}">
+                ${questions[number].answer[2].Answer}
               </label>
-            </div>
+            </div><br>
             <div class="form-check">
-              <input class="form-check-input" type="radio" name="answer" id="answer${i+4}">
-              <label class="form-check-label" for="answer${i+4}">
-                ${questions[i].answer[3].Answer}
+              <input class="form-check-input" type="radio" name="answer" id="answer${number + 4}">
+              <label class="form-check-label" for="answer${number + 4}">
+                ${questions[number].answer[3].Answer}
               </label>
+            </div><br>
+            <div class="card-footer text-end">
+                 <a href="#" class="btn btn-success" id="nextBtn">Next</a>
             </div>
         `;
-        $("#question").append(questionTemp);
-    }
-});
+    $("#question").append(questionTemp);
+}
